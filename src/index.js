94cb897a5a7c244396803e22e3ccd65cbbce499e
Rosenie Fenelon <fenelon.8@osu.edu>
2026-08-27 21:29:43 -0400

    Hello World

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import reportWebVitals from './reportWebVitals';
+function HelloWorld(){
+  return <h1>Hello, world!</h1>;
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelloWorld/>
   </React.StrictMode>
 );
 
